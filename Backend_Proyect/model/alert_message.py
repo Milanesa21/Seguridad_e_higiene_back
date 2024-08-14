12aa82bb9c3e85b0d077ba920dca1cc9bbb6bed5
Milanesa 21 <diegojara88574@gmail.com>
2024-08-14 15:46:51 -0300

    lo cambie a Jorgito

--- Backend_Proyect/model/alert_message.py
+++ Backend_Proyect/model/alert_message.py
@@ -1,15 +1,15 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
+from datetime import datetime
 from dataBase.db import Base
-from dataBase.db import engine
 from .user import Users
 
 class AlertMessage(Base):
     __tablename__ = "alert_messages"
 
     id_message = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    full_name = Column(String) 
+    user_id = Column(Integer, ForeignKey('users.id'), index=True)
+    full_name = Column(String, index=True)
     puesto_trabajo = Column(String)
     message = Column(String)
     
