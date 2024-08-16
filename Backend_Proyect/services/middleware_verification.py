e51a1a68d8ff822ef2a5d19423cbee803071f833
IvanPz01 <ivanciito_999@hotmail.com>
2024-08-16 16:28:57 -0300

    Creacion del la informacion mandada al front

--- Backend_Proyect/services/middleware_verification.py
+++ Backend_Proyect/services/middleware_verification.py
@@ -9,13 +9,31 @@ def get_user_info_by_id(user_id: int, db: Session = Depends(get_db)):
     user_by_id = get_user_by_id(user_id, db)
     if not user_by_id:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found or full_name does not match user_id")
+
+    if user_by_id['rol']['id'] == 1:
+        return {
+            "id": user_by_id['id'],
+            "rol": {
+                'nombre': user_by_id['rol']['nombre'],
+                'permisos': user_by_id['rol']['permisos']
+            }
+        }
+    if user_by_id['rol']['id'] == 2:
+        return {
+            "id": user_by_id['id'],
+            "full_name": user_by_id['full_name'],
+            "rol": {
+                'nombre': user_by_id['rol']['nombre'],
+                'permisos': user_by_id['rol']['permisos']
+            }
+        }
     return {
         "id": user_by_id['id'],
         "full_name": user_by_id['full_name'],
         "puesto_trabajo": user_by_id['puesto_trabajo'],
         "email": user_by_id['email'],
         "rol": {
-            'rol': user_by_id['rol']['nombre'],
+            'nombre': user_by_id['rol']['nombre'],
             'permisos': user_by_id['rol']['permisos']
         }
     }
