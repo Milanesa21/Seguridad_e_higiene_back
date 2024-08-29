789f6fdf18c3f78eeffaa4ade373aa73512187e4
Milanesa 21 <diegojara88574@gmail.com>
2024-08-29 10:18:03 -0300

    feat: :sparkles: Agregada la IA
    Se agregaron las rutas de llamado a las IA's de reconocimiento tanto de uniformes, como de ambiente de seguridad, pero se subieron comentados para no crear conflictos con los datos de otras ramas

--- Backend_Proyect/main.py
+++ Backend_Proyect/main.py
@@ -1,20 +1,21 @@
 from fastapi import FastAPI
-from routers.user_router import user_rutes
+from routers.user_router import user_routes
 from routers.company_router import company_rutes
 from routers.auth_router import auth_router
 from fastapi.middleware.cors import CORSMiddleware
 import init_db
-from services.Jorgito import app as jorgito_app
 from controllers.socket_controllers import router as socket_router  # Importa el router de WebSocket
 from services.roles_permisos_asignacion import Db_insert_RP
 from routers.permiso_router import permiso_router
+#from routers.IA_uniformes_routes import router as IA_uniformes_router
+#from routers.IA_ambiente_routes import router as IA_ambiente_router
 
 #Para iniciar el proyecto: uvicorn main:app --reload
 app = FastAPI()
 
 origins = [
-    "http://127.0.0.1:3000",  # Cambiado al puerto por defecto de React
-    "http://127.0.0.1:8000",  # Asegúrate de que el puerto es el correcto
+    "http://127.0.0.1:3000", 
+    "http://127.0.0.1:8000",  
 ]
 
 app.add_middleware(
@@ -25,12 +26,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(user_rutes)
+app.include_router(user_routes)
 app.include_router(auth_router)
 app.include_router(company_rutes)
-app.mount("/jorgito", jorgito_app)
-app.include_router(socket_router)  # Incluye el router de WebSocket
+app.include_router(socket_router)  
 app.include_router(permiso_router)
+#app.include_router(IA_uniformes_router)
+#app.include_router(IA_ambiente_router)
 
 Db_insert_RP()
 
